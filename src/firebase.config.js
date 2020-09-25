a9028f303d5cfbbb14926f132b286c64cc1caedf
Amir-16 <greenfitter516@gmail.com>
2020-09-25 23:51:11 +0600

    commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApPSROnfp67UQZjd1EJSSKBESoYEFFkrs",
+    authDomain: "travel-guru-15f15.firebaseapp.com",
+    databaseURL: "https://travel-guru-15f15.firebaseio.com",
+    projectId: "travel-guru-15f15",
+    storageBucket: "travel-guru-15f15.appspot.com",
+    messagingSenderId: "657122011464",
+    appId: "1:657122011464:web:2bbe0d34664552e117ab7d"
+  };
+  export default firebaseConfig
